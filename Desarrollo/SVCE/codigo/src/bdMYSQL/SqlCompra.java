f8cf9e4fde4fde3632107eef6b139626b157faaf
ErosAnaya1 <eros.anaya@unmsm.edu.pe>
2023-06-22 23:51:37 -0500

    Requisito 8
    modificaciones para SqlCompra

--- Desarrollo/SVCE/codigo/src/bdMYSQL/SqlCompra.java
+++ Desarrollo/SVCE/codigo/src/bdMYSQL/SqlCompra.java
@@ -6,7 +6,9 @@ package bdMYSQL;
 
 import Clases.Reporte;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,11 +17,15 @@ import java.util.List;
  * @author CESAR
  */
 public class SqlCompra extends ConexionBD{
-    public List<Reporte> obtenerPedido(){
+    public List<Reporte> obtenerPedido() throws SQLException{
         List<Reporte> art = new ArrayList<>();
         ResultSet rs;
         String sql = "SELECT * FROM reporte ";
         Connection con = conectar();
+        try (PreparedStatement ps = con.prepareStatement(sql)){
+            rs = ps.executeQuery();
+            while (rs.next()){
+            }
     }
 
     public List<Reporte> obtenerPedido() {
