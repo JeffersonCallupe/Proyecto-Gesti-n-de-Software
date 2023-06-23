a094ceb12551b502e13b806fe98443a52da48b84
LinanParedesSaul <saul.linan@unmsm.edu.pe>
2023-06-22 23:13:33 -0500

    Finalización REQ - 007

---
+++ Desarrollo/SVCE/Codigo/GCS/src/gcs/Carrito.java
@@ -0,0 +1,219 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
+ */
+package gcs;
+
+public class Carrito extends javax.swing.JFrame {
+
+    Catalogo frame;
+    public Carrito() {
+        initComponents();
+        setLocationRelativeTo(null);    
+        setResizable(false);
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        MetodosDePagos = new javax.swing.ButtonGroup();
+        jScrollBar1 = new javax.swing.JScrollBar();
+        jPanel = new javax.swing.JPanel();
+        jPanel2 = new javax.swing.JPanel();
+        jLabel1 = new javax.swing.JLabel();
+        pnlDatos = new javax.swing.JPanel();
+        lblDatos = new javax.swing.JLabel();
+        pnlMetodos = new javax.swing.JPanel();
+        lblMetodos = new javax.swing.JLabel();
+        jLabel4 = new javax.swing.JLabel();
+        jLabel5 = new javax.swing.JLabel();
+        jLabel6 = new javax.swing.JLabel();
+        jLabel7 = new javax.swing.JLabel();
+        txtNombre = new javax.swing.JTextField();
+        jLabel8 = new javax.swing.JLabel();
+        btnListaCompras = new javax.swing.JButton();
+        jSeparator2 = new javax.swing.JSeparator();
+        jSeparator3 = new javax.swing.JSeparator();
+        txtDNI = new javax.swing.JTextField();
+        jSeparator4 = new javax.swing.JSeparator();
+        txtCorreo = new javax.swing.JTextField();
+        jSeparator5 = new javax.swing.JSeparator();
+        txtDireccion = new javax.swing.JTextField();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jPanel.setBackground(new java.awt.Color(255, 255, 255));
+        jPanel.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        jPanel2.setBackground(new java.awt.Color(255, 153, 0));
+        jPanel2.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
+        jPanel2.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        jLabel1.setFont(new java.awt.Font("Segoe UI Black", 1, 20)); // NOI18N
+        jLabel1.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel1.setText("CARRITO DE COMPRAS");
+        jPanel2.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(300, 0, -1, 40));
+
+        jPanel.add(jPanel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 817, -1));
+
+        pnlDatos.setBackground(new java.awt.Color(255, 153, 0));
+        pnlDatos.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
+        pnlDatos.setForeground(new java.awt.Color(255, 153, 0));
+
+        lblDatos.setBackground(new java.awt.Color(255, 255, 255));
+        lblDatos.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
+        lblDatos.setForeground(new java.awt.Color(255, 255, 255));
+        lblDatos.setText("DATOS");
+
+        javax.swing.GroupLayout pnlDatosLayout = new javax.swing.GroupLayout(pnlDatos);
+        pnlDatos.setLayout(pnlDatosLayout);
+        pnlDatosLayout.setHorizontalGroup(
+            pnlDatosLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(pnlDatosLayout.createSequentialGroup()
+                .addGap(55, 55, 55)
+                .addComponent(lblDatos)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+        pnlDatosLayout.setVerticalGroup(
+            pnlDatosLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, pnlDatosLayout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(lblDatos)
+                .addContainerGap())
+        );
+
+        jPanel.add(pnlDatos, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 80, -1, -1));
+
+        pnlMetodos.setBackground(new java.awt.Color(255, 153, 0));
+        pnlMetodos.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
+        pnlMetodos.setForeground(new java.awt.Color(255, 153, 0));
+
+        lblMetodos.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
+        lblMetodos.setForeground(new java.awt.Color(255, 255, 255));
+        lblMetodos.setText("MÉTODO DE PAGO");
+
+        javax.swing.GroupLayout pnlMetodosLayout = new javax.swing.GroupLayout(pnlMetodos);
+        pnlMetodos.setLayout(pnlMetodosLayout);
+        pnlMetodosLayout.setHorizontalGroup(
+            pnlMetodosLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(pnlMetodosLayout.createSequentialGroup()
+                .addGap(58, 58, 58)
+                .addComponent(lblMetodos)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+        pnlMetodosLayout.setVerticalGroup(
+            pnlMetodosLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(pnlMetodosLayout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(lblMetodos)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+
+        jPanel.add(pnlMetodos, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 345, -1, -1));
+
+        jLabel4.setFont(new java.awt.Font("Segoe UI", 0, 13)); // NOI18N
+        jLabel4.setText("Nombre");
+        jPanel.add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(120, 160, -1, -1));
+
+        jLabel5.setFont(new java.awt.Font("Segoe UI", 3, 12)); // NOI18N
+        jLabel5.setForeground(new java.awt.Color(204, 204, 204));
+        jLabel5.setText("Indique los datos de la persona quien recepcionará los productos");
+        jPanel.add(jLabel5, new org.netbeans.lib.awtextra.AbsoluteConstraints(90, 130, -1, -1));
+
+        jLabel6.setFont(new java.awt.Font("Segoe UI", 0, 13)); // NOI18N
+        jLabel6.setText("DNI");
+        jPanel.add(jLabel6, new org.netbeans.lib.awtextra.AbsoluteConstraints(120, 200, -1, -1));
+
+        jLabel7.setFont(new java.awt.Font("Segoe UI", 0, 13)); // NOI18N
+        jLabel7.setText("Dirección");
+        jPanel.add(jLabel7, new org.netbeans.lib.awtextra.AbsoluteConstraints(120, 280, -1, -1));
+
+        txtNombre.setBorder(null);
+        jPanel.add(txtNombre, new org.netbeans.lib.awtextra.AbsoluteConstraints(180, 160, 350, 20));
+
+        jLabel8.setFont(new java.awt.Font("Segoe UI", 0, 13)); // NOI18N
+        jLabel8.setText("Correo");
+        jPanel.add(jLabel8, new org.netbeans.lib.awtextra.AbsoluteConstraints(120, 240, -1, -1));
+
+        btnListaCompras.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/bienes (1).png"))); // NOI18N
+        btnListaCompras.setBorder(null);
+        btnListaCompras.setBorderPainted(false);
+        btnListaCompras.setContentAreaFilled(false);
+        btnListaCompras.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnListaComprasActionPerformed(evt);
+            }
+        });
+        jPanel.add(btnListaCompras, new org.netbeans.lib.awtextra.AbsoluteConstraints(640, 200, 70, 70));
+
+        jSeparator2.setForeground(new java.awt.Color(0, 0, 0));
+        jPanel.add(jSeparator2, new org.netbeans.lib.awtextra.AbsoluteConstraints(180, 180, 350, 20));
+
+        jSeparator3.setForeground(new java.awt.Color(0, 0, 0));
+        jPanel.add(jSeparator3, new org.netbeans.lib.awtextra.AbsoluteConstraints(180, 220, 350, 20));
+
+        txtDNI.setBorder(null);
+        jPanel.add(txtDNI, new org.netbeans.lib.awtextra.AbsoluteConstraints(180, 200, 350, 20));
+
+        jSeparator4.setForeground(new java.awt.Color(0, 0, 0));
+        jPanel.add(jSeparator4, new org.netbeans.lib.awtextra.AbsoluteConstraints(180, 260, 350, 20));
+
+        txtCorreo.setBorder(null);
+        jPanel.add(txtCorreo, new org.netbeans.lib.awtextra.AbsoluteConstraints(180, 240, 350, 20));
+
+        jSeparator5.setForeground(new java.awt.Color(0, 0, 0));
+        jPanel.add(jSeparator5, new org.netbeans.lib.awtextra.AbsoluteConstraints(180, 300, 350, 20));
+
+        txtDireccion.setBorder(null);
+        jPanel.add(txtDireccion, new org.netbeans.lib.awtextra.AbsoluteConstraints(180, 280, 350, 20));
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addComponent(jPanel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 0, Short.MAX_VALUE))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addComponent(jPanel, javax.swing.GroupLayout.PREFERRED_SIZE, 644, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 0, Short.MAX_VALUE))
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void btnListaComprasActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnListaComprasActionPerformed
+
+// TODO add your handling code here:
+    }//GEN-LAST:event_btnListaComprasActionPerformed
+
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    public static javax.swing.ButtonGroup MetodosDePagos;
+    private javax.swing.JButton btnListaCompras;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
+    private javax.swing.JLabel jLabel6;
+    private javax.swing.JLabel jLabel7;
+    private javax.swing.JLabel jLabel8;
+    private javax.swing.JPanel jPanel;
+    private javax.swing.JPanel jPanel2;
+    private javax.swing.JScrollBar jScrollBar1;
+    private javax.swing.JSeparator jSeparator2;
+    private javax.swing.JSeparator jSeparator3;
+    private javax.swing.JSeparator jSeparator4;
+    private javax.swing.JSeparator jSeparator5;
+    private javax.swing.JLabel lblDatos;
+    private javax.swing.JLabel lblMetodos;
+    private javax.swing.JPanel pnlDatos;
+    private javax.swing.JPanel pnlMetodos;
+    private javax.swing.JTextField txtCorreo;
+    private javax.swing.JTextField txtDNI;
+    private javax.swing.JTextField txtDireccion;
+    private javax.swing.JTextField txtNombre;
+    // End of variables declaration//GEN-END:variables
+}
