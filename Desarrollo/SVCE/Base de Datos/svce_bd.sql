0095223ac440e47b659ae27a647e18a25f6074ec
Cristhian Paolo <98907409+Cristh715@users.noreply.github.com>
2023-07-02 21:05:35 -0500

    Add files via upload

--- Desarrollo/SVCE/Base de Datos/svce_bd.sql
+++ Desarrollo/SVCE/Base de Datos/svce_bd.sql
@@ -32,18 +32,8 @@ CREATE TABLE `cliente` (
   `direccion` varchar(50) DEFAULT NULL,
   `ciudad` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`id_cliente`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `cliente`
---
-
-LOCK TABLES `cliente` WRITE;
-/*!40000 ALTER TABLE `cliente` DISABLE KEYS */;
-INSERT INTO `cliente` VALUES (2,'Cristhian','Atuncar',NULL,'923706131','cristhian.atuncar@gamarrita.com',NULL,NULL);
-/*!40000 ALTER TABLE `cliente` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `producto`
@@ -60,18 +50,29 @@ CREATE TABLE `producto` (
   `stock` int DEFAULT NULL,
   `tipo` varchar(100) DEFAULT NULL,
   `imagen` varchar(250) DEFAULT NULL,
-  PRIMARY KEY (`id_producto`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `id_promocion` int DEFAULT NULL,
+  PRIMARY KEY (`id_producto`),
+  KEY `id_promocion` (`id_promocion`),
+  CONSTRAINT `id_promocion` FOREIGN KEY (`id_promocion`) REFERENCES `promocion` (`id_promocion`)
+) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `producto`
+-- Table structure for table `promocion`
 --
 
-LOCK TABLES `producto` WRITE;
-/*!40000 ALTER TABLE `producto` DISABLE KEYS */;
-/*!40000 ALTER TABLE `producto` ENABLE KEYS */;
-UNLOCK TABLES;
+DROP TABLE IF EXISTS `promocion`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `promocion` (
+  `id_promocion` int NOT NULL AUTO_INCREMENT,
+  `fecha_inicio` date DEFAULT NULL,
+  `fecha_fin` date DEFAULT NULL,
+  `tipo_promocion` varchar(250) DEFAULT NULL,
+  `descuento` int DEFAULT NULL,
+  PRIMARY KEY (`id_promocion`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Table structure for table `usuario`
@@ -89,18 +90,8 @@ CREATE TABLE `usuario` (
   PRIMARY KEY (`id_usuario`),
   KEY `id_cliente` (`id_cliente`),
   CONSTRAINT `usuario_ibfk_1` FOREIGN KEY (`id_cliente`) REFERENCES `cliente` (`id_cliente`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `usuario`
---
-
-LOCK TABLES `usuario` WRITE;
-/*!40000 ALTER TABLE `usuario` DISABLE KEYS */;
-INSERT INTO `usuario` VALUES (1,2,'cristhian.atuncar@gamarrita.com','68378d409dc4d373f2abdc0bc6fc24b52b6ba19579b0a6d6346e04ec7122aa76','cliente');
-/*!40000 ALTER TABLE `usuario` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `venta`
@@ -115,22 +106,14 @@ CREATE TABLE `venta` (
   `id_producto` int NOT NULL,
   `fecha` date DEFAULT NULL,
   `cantidad` varchar(250) DEFAULT NULL,
+  `precio` float DEFAULT NULL,
   PRIMARY KEY (`id_venta`),
   KEY `id_cliente` (`id_cliente`),
   KEY `id_producto` (`id_producto`),
   CONSTRAINT `venta_ibfk_1` FOREIGN KEY (`id_cliente`) REFERENCES `cliente` (`id_cliente`),
   CONSTRAINT `venta_ibfk_2` FOREIGN KEY (`id_producto`) REFERENCES `producto` (`id_producto`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=24 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `venta`
---
-
-LOCK TABLES `venta` WRITE;
-/*!40000 ALTER TABLE `venta` DISABLE KEYS */;
-/*!40000 ALTER TABLE `venta` ENABLE KEYS */;
-UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -141,4 +124,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-06-04 10:54:35
+-- Dump completed on 2023-07-02 20:48:51
